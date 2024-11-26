d6aefc466fc6c19ce07a4cbb5c89ae29182d7038
ruanzhixiong <ruanzhixiong@bobandata.com>
2024-11-26 18:21:27 +0800

    fix: 修改文件名大小写

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
 <script setup lang="ts">
-import HelloWorld from '@/components/HelloWorld.vue'
+// https://github.com/vueuse/head
+// you can use this to manipulate the document head in any components,
+// they will be rendered correctly in the html results with vite-ssg
+useHead({
+  title: 'Vitesse',
+  meta: [
+    {
+      name: 'description',
+      content: 'Opinionated Vite Starter Template',
+    },
+    {
+      name: 'theme-color',
+      content: () => (isDark.value ? '#00aba9' : '#ffffff'),
+    },
+  ],
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/svg+xml',
+      href: () => (preferredDark.value ? '/favicon-dark.svg' : '/favicon.svg'),
+    },
+  ],
+})
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue + TS" />
+  <RouterView />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,45 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import Layouts from 'vite-plugin-vue-layouts'
 import Inspect from 'vite-plugin-inspect'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
-import path from 'path'
-
+import { VitePWA } from 'vite-plugin-pwa'
+import VueMacros from 'unplugin-vue-macros/vite'
+import WebfontDownload from 'vite-plugin-webfont-dl'
+import VueRouter from 'unplugin-vue-router/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
+import VueDevTools from 'vite-plugin-vue-devtools'
 const resolve = (dir: string) => path.join(__dirname, dir)
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve('src'),
+    },
+  },
   plugins: [
-    vue({
-      script: {
-        defineModel: true,
-        propsDestructure: true, // 解构 props
+    // https://github.com/posva/unplugin-vue-router
+    VueRouter({
+      extensions: ['.vue', '.md'],
+      dts: './typed-router.d.ts',
+    }),
+    VueMacros({
+      plugins: {
+        vue: vue({
+          include: [/\.vue$/, /\.md$/],
+          script: {
+            defineModel: true,
+            propsDestructure: true, // 解构 props
+          },
+        }),
+        vueJsx: vueJsx(),
       },
     }),
-    vueJsx(),
+    // https://github.com/JohnCampionJr/vite-plugin-vue-layouts
+    Layouts(),
     Inspect(),
     AutoImport({
       include: [
@@ -25,13 +48,22 @@ export default defineConfig({
         /\.vue\?vue/, // .vue
         /\.md$/, // .md
       ],
-      imports: ['vue', 'vue-router'], // 自动导入vue和vue-router等相关函数
+      imports: [
+        'vue',
+        '@vueuse/head',
+        '@vueuse/core',
+        VueRouterAutoImports,
+        {
+          // add any other imports you were relying on
+          'vue-router/auto': ['useLink'],
+        },
+      ], // 自动导入vue和vue-router等相关函数
       eslintrc: {
         enabled: false, // 若没此json文件，先开启，生成后在关闭
         filepath: './.eslintrc-auto-import.json', // 默认
         globalsPropValue: true,
       },
-      dirs: ['src/store'],
+      dirs: ['src/store', 'src/composables'],
       vueTemplate: true,
       resolvers: [],
     }),
@@ -43,10 +75,44 @@ export default defineConfig({
         /\.vue\?vue/, // .vue
         /\.md$/, // .md
       ],
+      extensions: ['vue', 'md'],
       dirs: ['src/components'],
       deep: true,
       resolvers: [],
     }),
+    // https://github.com/antfu/vite-plugin-pwa
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'safari-pinned-tab.svg'],
+      manifest: {
+        name: 'Vitesse',
+        short_name: 'Vitesse',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+    // https://github.com/feat-agency/vite-plugin-webfont-dl
+    WebfontDownload(),
+
+    // https://github.com/webfansplz/vite-plugin-vue-devtools
+    VueDevTools(),
   ],
   server: {
     port: 3334,
@@ -58,11 +124,6 @@ export default defineConfig({
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
-    },
-  },
-  resolve: {
-    alias: {
-      '@': resolve('src'),
     },
   },
 })
